18ae8b09ef350a871721e34795c16fc642b3c38a
mitul bipin <mitulbipin24@gmail.com>
2021-04-05 23:16:01 +0530

    Formatted the code

--- src/com/java/security/framework/algorithm/algorithmList.java
+++ src/com/java/security/framework/algorithm/algorithmList.java
@@ -4,13 +4,16 @@ import java.util.HashMap;
 import com.java.security.framework.common.ConstantsUtils;
 
 public class algorithmList {
-	public static HashMap<String, String> listOfAlgorithms() {
+	public static void listOfAlgorithms() {
 
 		HashMap<String, String> algorithmList = new HashMap<String, String>();
-		algorithmList.put("3", ConstantsUtils.BasicEncryptoText);
-		algorithmList.put("2", ConstantsUtils.SubstitutionAlgorithmText);
 		algorithmList.put("1", ConstantsUtils.RSAlgorithmText);
-
-		return algorithmList;
+		algorithmList.put("2", ConstantsUtils.SubstitutionAlgorithmText);
+		algorithmList.put("3", ConstantsUtils.BasicEncryptoText);
+		
+		System.out.println("\nList of algorithms available:");
+		for(String i : algorithmList.keySet())
+			System.out.println(i+". " + algorithmList.get(i)); 
 	}
+
 }
--- src/com/java/security/framework/encrypto/BuildClass.java
+++ src/com/java/security/framework/encrypto/BuildClass.java
@@ -10,18 +10,36 @@ public class BuildClass {
 
 	static Scanner input = new Scanner(System.in);
 	static IEncryptionImpl crypto = new IEncryptionImpl();
+	static String data;
+	static int option;
 
 	public static void main(String args[]) throws Exception {
 		System.out.println(ConstantsUtils.frameworkTitle);
 		System.out.println(ConstantsUtils.frameworkLines_Horizontal);
 		System.out.print("\n" + ConstantsUtils.inputText);
-		String data = input.next();
+		data = input.next();
+		algorithmList.listOfAlgorithms();
 		System.out.println("\n" + ConstantsUtils.frameworkLines_Horizontal);
-		System.out.print(algorithmList.listOfAlgorithms());
+		algorithmSelector();
 
-//		AlgorithmOutput.basicEncryptoAlgorithm(data);
-//		AlgorithmOutput.substitutionAlgorithm(data);
-		AlgorithmOutput.RSAEncryptionAlgorithm(data);
+	}
+	
+	public static void algorithmSelector() throws Exception  {
+		System.out.print("\n" + ConstantsUtils.userInputText);
+		option = input.nextInt();
+		switch (option) {
+		case 1:
+			AlgorithmOutput.RSAEncryptionAlgorithm(data);
+			break;
+		case 2:
+			AlgorithmOutput.substitutionAlgorithm(data);
+			break;
+		case 3:
+			AlgorithmOutput.basicEncryptoAlgorithm(data);
+			break;
+		default:
+			System.out.println("Algorithm not available/ incorrect option");
 
+		}
 	}
 }
